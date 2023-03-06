2ea17c97b34a6303d185b85ee5f505dd7689980b
Jan Acuna <jan.acuna@crowdbotics.com>
2023-03-06 15:55:05 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,4 @@
+import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -9,12 +10,19 @@ const Untitled2 = () => {
       position: 'relative',
       flex: 1
     }}></ScrollView>
-    </SafeAreaView>;
+    <View style={styles.RBrCIMuG}></View></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  RBrCIMuG: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default Untitled2;
