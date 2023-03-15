4cd7919e29156adf59e3697cfb80e1d3c2c7cb0c
Jan Acuna <jan.acuna@crowdbotics.com>
2023-03-15 16:36:57 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,8 +2,8 @@ export const globalOptions = {
   "name": "test-jan-001-dev-61080",
   "url": "https://test-jan-001-dev-61080.botics.co",
   "api": "https://test-jan-001-dev-61080.botics.co/api/v1",
-  "initialRoute": "Untitled6",
-  "initialRoute": "Untitled6"
+  "initialRoute": "Untitled3",
+  "initialRoute": "Untitled3"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
