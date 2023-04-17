a54c15c7e20610a0b83d3cb47af68857a6b4a6af
Jan Acuna <jan.acuna@crowdbotics.com>
2023-04-17 22:13:50 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled7/index.js
@@ -0,0 +1,55 @@
+import { TextInput } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled7 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><TextInput style={styles.BGyfgvjD} value="Hola mundo"></TextInput><TextInput style={styles.cEjEyBtR}></TextInput><TextInput style={styles.pkWVOfSY}></TextInput></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  SEEtXMtF: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  BGyfgvjD: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30,
+    position: "absolute",
+    left: 7,
+    top: 91
+  },
+  cEjEyBtR: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30,
+    position: "absolute",
+    left: 7,
+    top: 207
+  },
+  pkWVOfSY: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30,
+    position: "absolute",
+    left: 8,
+    top: 145
+  }
+});
+export default Untitled7;
