a00fe21d327eac23da2e25c5da7de3b43ec42d64
Jan Acuna <jan.acuna@crowdbotics.com>
2023-04-18 09:27:56 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled5/index.js
+++ screens/Untitled5/index.js
@@ -8,7 +8,7 @@ const Untitled5 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><View style={styles.hSBfYFeZ}></View></ScrollView>
+    }}><View style={styles.hSBfYFeZ}></View><View style={styles.nTPzJXIu}></View><View style={styles.dYoaWBtZ}></View></ScrollView>
     </SafeAreaView>;
 };
 
@@ -20,6 +20,20 @@ const styles = StyleSheet.create({
     height: 60,
     width: 318,
     backgroundColor: "#f40000",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  nTPzJXIu: {
+    height: 158,
+    width: 310,
+    backgroundColor: "#1a0bbd",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  dYoaWBtZ: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
     borderRadius: 0,
     color: "#777777"
   }
