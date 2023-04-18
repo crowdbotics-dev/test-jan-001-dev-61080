7ec94c9a4ae216cde2aac67b0bc28ee4e1293007
Jan Acuna <jan.acuna@crowdbotics.com>
2023-04-18 09:16:26 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled8/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled8 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><View style={styles.rXpDryTT}></View><View style={styles.ByUADIZh}></View><View style={styles.mzjEaDCt}></View></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  rXpDryTT: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  ByUADIZh: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#ff0000",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  mzjEaDCt: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
+export default Untitled8;
