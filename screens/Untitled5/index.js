d2906bc882c1cfb146b203ad05b7397dfd8b07a0
Jan Acuna <jan.acuna@crowdbotics.com>
2023-04-18 09:26:20 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled5/index.js
+++ screens/Untitled5/index.js
@@ -1,5 +1,4 @@
 import { View } from "react-native";
-import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled5 = () => {
@@ -9,20 +8,13 @@ const Untitled5 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><View style={styles.IDNJCRpc}></View><View style={styles.hSBfYFeZ}></View></ScrollView>
+    }}><View style={styles.hSBfYFeZ}></View></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
-  },
-  IDNJCRpc: {
-    height: 60,
-    width: 140,
-    backgroundColor: "#E4E4E4",
-    borderRadius: 0,
-    color: "#777777"
   },
   hSBfYFeZ: {
     height: 60,
